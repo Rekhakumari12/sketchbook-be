abc4b8159ca487f9e23d574f7b18c8a7d41171fa
Rekha Kumari <rekhakumari@Rekhas-MacBook-Air.local>
2024-11-28 13:38:05 +0530

    provide the enf point

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const app = require('express')()
 const httpServer = require('http').createServer(app)
 const cors = require('cors')
+
+const isDev = app.settings.env === 'development'
+const URL = isDev ? 'http://localhost:3000/' : 'https://sketchbook-fe-pi.vercel.app/'
 const options = {
-  cors: 'http://localhost:3000/',
+  cors: URL,
 }
 
-app.use(cors({ origin: 'http://localhost:3000/' })) //whitelisting client
+app.use(cors({ origin: URL })) //whitelisting client
 const io = require('socket.io')(httpServer, options)
 
 io.on('connection', (socket) => {
